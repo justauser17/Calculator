5acb022328f913403c76c692fe7d408bce27463a
justauser17 <113825867+justauser17@users.noreply.github.com>
2023-06-17 00:52:59 +1000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,75 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
+
+    private static final String[] romanNum = {"C", "XC", "L", "XL", "X", "IX","VIII", "VII", "VI", "V", "IV", "III", "II", "I"};
+    private static final int[] num = {100, 90, 50, 40, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(calc(input));
     }
+
+    public static int romanToArab(String romanNumber) {
+
+        int arabNum = 0;
+        for (int i = 0; i < num.length; i++){
+            while (romanNumber.startsWith(romanNum[i])){
+                arabNum += num[i];
+                romanNumber = romanNumber.substring(romanNum[i].length());
+            }
+        }
+        return arabNum;
+    }
+
+    public static String arabToRoman(int arabNum) {
+
+        StringBuilder roman = new StringBuilder();
+        for (int i = 0; i < num.length; i++) {
+            while (arabNum >= num[i]) {
+                roman.append(romanNum[i]);
+                arabNum -= num[i];
+            }
+        }
+        return roman.toString();
+    }
+
+    private static boolean isRoman(String s) {
+        for (String romanNumeral : romanNum) {
+            if (s.equals(romanNumeral)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String calc(String input){
+        String[] array = input.split(" ");
+        int num1, num2;
+        String operator = array[1];
+
+        if (isRoman(array[0])) {
+            num1 = romanToArab(array[0]);
+            num2 = romanToArab(array[2]);
+        } else {
+            num1 = Integer.parseInt(array[0]);
+            num2 = Integer.parseInt(array[2]);
+        }
+
+        int result;
+        if (num1 > 0 && num1 <= 10 && num2 > 0 && num2 <= 10) {
+            result = switch (operator) {
+                case "+" -> num1 + num2;
+                case "-" -> num1 - num2;
+                case "*" -> num1 * num2;
+                case "/" -> num1 / num2;
+                default -> throw new IllegalArgumentException("неверный знак");
+            };
+        } else throw new NumberFormatException("неверный диапазон чисел");
+
+        if (isRoman(array[0])) return arabToRoman(result);
+        return Integer.toString(result);
+    }
+
 }
